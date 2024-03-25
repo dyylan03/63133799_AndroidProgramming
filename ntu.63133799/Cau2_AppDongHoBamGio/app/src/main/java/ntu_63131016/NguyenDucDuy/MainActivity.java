7cc7b1af0723d6a6f82ceb6a2e5df5dc6a156466
Nguyen Duy <nguyenducduymmyn@gmail.com>
2024-03-26 00:18:05 +0700

    Them Header

--- ntu.63133799/Cau2_AppDongHoBamGio/app/src/main/java/ntu_63131016/NguyenDucDuy/MainActivity.java
+++ ntu.63133799/Cau2_AppDongHoBamGio/app/src/main/java/ntu_63131016/NguyenDucDuy/MainActivity.java
@@ -31,7 +31,67 @@ public class MainActivity extends AppCompatActivity {
         buttonPause = findViewById(R.id.buttonPause);
         buttonReset = findViewById(R.id.buttonReset);
         buttonLap = findViewById(R.id.buttonLap);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTime = System.currentTimeMillis();
+                handler.postDelayed(updateTimer, 0);
+                buttonReset.setEnabled(false);
+            }
+        });
+
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff += timeInMilliseconds;
+                handler.removeCallbacks(updateTimer);
+                buttonReset.setEnabled(true);
+            }
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timeSwapBuff = 0L;
+                startTime = 0L;
+                textViewTime.setText("00:00:00");
+                lapNumber = 1;
+                LinearLayout linearLayoutLaps = findViewById(R.id.linearLayoutLaps);
+                linearLayoutLaps.removeAllViews();
+            }
+        });
+
+        buttonLap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addLapTime();
+            }
+        });
+    }
+
+    private Runnable updateTimer = new Runnable() {
+        public void run() {
+            timeInMilliseconds = System.currentTimeMillis() - startTime;
+            updatedTime = timeSwapBuff + timeInMilliseconds;
+
+            int hours = (int) (updatedTime / (1000 * 60 * 60));
+            int minutes = (int) (updatedTime % (1000 * 60 * 60)) / (1000 * 60);
+            int seconds = (int) ((updatedTime % (1000 * 60 * 60)) % (1000 * 60) / 1000);
+            int milliseconds = (int) ((updatedTime % (1000 * 60 * 60)) % (1000 * 60) % 1000);
+
+            String timeFormatted = String.format("%02dh: %02dm: %02ds: %03dms", hours, minutes, seconds, milliseconds);
+            textViewTime.setText(timeFormatted);
+            handler.postDelayed(this, 0);
+        }
+    };
 
 
+    private void addLapTime() {
+        LinearLayout linearLayoutLaps = findViewById(R.id.linearLayoutLaps);
+        TextView lapTextView = new TextView(this);
+        lapTextView.setText("Vòng " + lapNumber + ": " + textViewTime.getText());
+        lapTextView.setTextSize(20);
+        linearLayoutLaps.addView(lapTextView);
+        lapNumber++;
     }
 }
