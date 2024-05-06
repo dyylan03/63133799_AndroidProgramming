f653f95c1ba722b9479a3a9ab1493f5b5ee3f669
Nguyen Duy <nguyenducduymmyn@gmail.com>
2024-05-07 04:00:31 +0700

    finishIntent

--- viduIntent/app/src/main/java/com/example/vidu1intent/MainActivity.java
+++ viduIntent/app/src/main/java/com/example/vidu1intent/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.vidu1intent;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -14,10 +16,10 @@ public class MainActivity extends AppCompatActivity {
     Button nutMH2;
     Button nutMH3;
     void timDieuKien(){
-        nutMH2 = (Button) findViewById(R.id.btnMH2);
-        nutMH3 = (Button) findViewById(R.id.btnMH3);
+        nutMH2 = findViewById(R.id.btnMH2);
+        nutMH3 = findViewById(R.id.btnMH3);
     }
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +33,23 @@ public class MainActivity extends AppCompatActivity {
 
         });
         timDieuKien();
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
 
+                startActivity(intentMH2);
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+
+
+                startActivity(intentMH3);
+            }
+        });
 
 
 
